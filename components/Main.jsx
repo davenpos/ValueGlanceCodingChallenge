6c3d0aa953ed0f538d907d9165016d7009fe25d8
davenpos <simmywim@hotmail.com>
2025-08-21 13:43:56 -0400

    Installed Chart.js packages and created Table component

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,6 +1,9 @@
 'use client';
 import Graph from '@/components/Graph';
-import { useState, useEffect } from 'react';
+import Table from '@/components/Table';
+import { useState, useEffect, createContext } from 'react';
+
+export const PricesContext = createContext();
 
 export default function Main() {
   const [prices, setPrices] = useState([]);
@@ -8,11 +11,18 @@ export default function Main() {
   useEffect(() => {
     (async function fetchPrices() {
       const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/prices`);
-      const data = await res.json();
-      console.log(data);
-      setPrices(data.patients);
+      const { prices } = await res.json();
+      const timeSeries = prices['Time Series (5min)'] || {};
+      const labels = Object.keys(timeSeries).sort();
+      const data = labels.map((timestamp) => parseFloat(timeSeries[timestamp]['4. close']));
+      setPrices({ labels, data });
     })();
   }, []);
 
-  return <Graph />;
+  return (
+    <PricesContext.Provider value={prices}>
+      <Graph />
+      <Table />
+    </PricesContext.Provider>
+  );
 }
---
+++ components/Table.jsx
@@ -0,0 +1,18 @@
+export default function Table() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Header 1</th>
+          <th>Header 2</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Data 1</td>
+          <td>Data 2</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
